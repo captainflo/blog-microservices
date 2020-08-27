540996c5be5e16736e1cb0bac72b53858aa0a53a
Florian Lahitte <lahitte.florian@gmail.com>
2020-08-27 12:17:41 -0400

    microservice

--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { randomBytes } = require('crypto');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,7 +13,7 @@ app.get('/posts', (req, res) => {
   res.send(posts);
 });
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
   // random ID
   const id = randomBytes(4).toString('hex');
   const { title } = req.body;
@@ -21,8 +22,18 @@ app.post('/posts', (req, res) => {
     id,
     title,
   };
+  await axios.post('http://localhost:4005/events', {
+    type: 'PostCreated',
+    data: { id, title },
+  });
 
   res.status(201).send(posts[id]);
+});
+
+app.post('/events', async (req, res) => {
+  console.log('Received Event', req.body);
+
+  res.send({});
 });
 
 app.listen(4000, () => {
